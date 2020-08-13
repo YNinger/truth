51d2675e887191338afd66f4e9b4e9177c795e52
YNinger <1002272987@qq.com>
2020-08-13 22:06:31 +0800

    while简单循环以及输出奇数

---
+++ Project20_08_13/Project20_08_13/text.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//	
+//	while (i <= 100)      //while语句循环
+//	{
+//		if (i % 2 == 1)
+//			printf(" %d", i);
+//		i++;
+//
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1)
+//		printf("奇数\n");	
+//	else
+//		printf("不是奇数\n");
+//		return 0;
+//}
