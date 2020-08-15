67cb8190c6b2a1aa9e5e0f7e8a532d8e06570c3e
YNinger <1002272987@qq.com>
2020-08-15 22:16:38 +0800

    “=”与“==”的区别细心观看，以及从大到小输出三个数的问题（前）

---
+++ Project20_08_15/Project20_08_15/zuoye.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//1
+#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i<10; i++)
+//	{
+//		if (i = 5)  //注意此时的“=”不是等于的意思，是赋值！！！
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d%d\n", &a, &b, &c);
+	if (a > b)
+	{
+
+	}
+		return 0;
+}
