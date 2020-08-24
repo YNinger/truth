de34d83196efa419ef6ad6fad8211e368527a3da
YNinger <1002272987@qq.com>
2020-08-24 21:17:20 +0800

    计算n的阶乘以及打印字符

---
+++ Project20_08_24/Project20_08_24/txet.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//int main()
+//{
+//	int ch = 0; //打印字符
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')//不代表数字0，9，仅代表ASKⅡ
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+//计算n的阶乘
+//int main()
+//{
+//	int n = 0;
+//	int i = 1;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
+
