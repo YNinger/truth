4b8732f2d8576f1db86ecc675a9056d15847c09d
YNinger <1002272987@qq.com>
2020-09-12 23:28:45 +0800

    十个整数的最大值

---
+++ Project20_09_12/Project20_09_12/text.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+////找出十个整数的最大值
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);//这个算出的sz大小针对所有数组大小，不仅是10
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		scanf("%d", &arr[i]);//输入数组，前面定义十个，所以输入十个数才有效
+//	}
+//	//以上步骤为输入数组元素
+//	int max = arr[i];
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i]>max)
+//		{
+//			max = arr[i];//每次保存的max均为上一次的，直到输入最后一个数结束
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+
+
