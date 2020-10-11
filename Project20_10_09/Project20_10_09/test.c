5496207b84e82a7c525fc1eec730ccd8cb3b89fe
YNinger <1002272987@qq.com>
2020-10-11 16:40:52 +0800

    打印二维数组和九九乘法表完整版

---
+++ Project20_10_09/Project20_10_09/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+////99乘法表
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i;j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+////打印二维数组  二维数组 行可以省略，列不可以省略
+//int main()
+//{
+//	int arr[3][4] = { { 1, 2 }, { 3, 4 }, 5 };
+//	int i = 0;//行数
+//	int j = 0;//列数
+//
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
