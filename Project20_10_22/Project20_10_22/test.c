3f57cb57a9bcc8c1fd2c342d1afd73a1f4e8edb6
YNinger <1002272987@qq.com>
2020-10-23 22:24:22 +0800

    ENTER NEW POINT FIRST 指针计算

---
+++ Project20_10_22/Project20_10_22/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	char**cp[] = { c + 3, c + 2, c + 1, c };
+	char***cpp = cp;
+	//前两行改变了cpp的值
+	printf("%s\n", **++cpp);
+	printf("%s\n", *--*++cpp + 3);//
+
+
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	
+
+	return 0;
+}
