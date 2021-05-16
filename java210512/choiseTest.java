7d501e89971b0a48149c11ecd05776e94364894c
YNinger <69383808+YNinger@users.noreply.github.com>
2021-05-16 18:43:10 +0800

    Add files via upload

---
+++ java210512/choiseTest.java
@@ -0,0 +1,14 @@
+package java210512;
+
+public class choiseTest {
+    public static void main(String[] args) {
+        System.out.printf("%d\n",foo(20,13));
+    }
+
+    public static int foo(int x, int y) {
+        if(x<0||y<0){
+            return 1;
+        }
+        return 3*foo(x-6,y/2);
+    }
+}
