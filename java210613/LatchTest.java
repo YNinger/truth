3da364cb7155e125cdc88e395c503ba04b33baf5
YNinger <69383808+YNinger@users.noreply.github.com>
2021-06-14 23:19:18 +0800

    马戏团&&多线程

---
+++ java210613/LatchTest.java
@@ -0,0 +1,26 @@
+package java210613;
+
+import java.util.concurrent.CountDownLatch;
+
+public class LatchTest {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            final int j = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(j);
+                    }finally {
+                        //线程结束
+                        latch.countDown();
+                    }
+                }
+            }).start();
+        }
+        //等待
+        latch.await();
+        System.out.println("main");
+    }
+}
